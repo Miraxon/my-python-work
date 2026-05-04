face313529e2bab4d59a8156b0ac904e45449393
Miraxon <miracle.mirak@gmail.com>
2026-05-04 11:03:45 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,12 @@
+apples= int(input("whats the apples price?\n"))
+oranges= int(input("whats the oranges price?\n"))
+bananas= int(input("whats the banans price?\n"))
+budget= 100
+total= apples + oranges +bananas
+diff=budget-total
+if diff > 0:
+    print(f"cheap there is still  {diff}$ left")
+elif diff <0:
+    print(f"too expensive ,{abs(diff)}$ more is needed")
+else:
+    print("exact there is no money left")
