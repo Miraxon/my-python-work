2407ed3c97418b7c7376745dd5eb8e94e12ad1b2
Miraxon <miracle.mirak@gmail.com>
2026-05-07 02:46:31 +0300

    Add files via upload

---
+++ numbers divisble by 3.py
@@ -0,0 +1,5 @@
+list=[]
+for n in range(1,21):
+    if n%3==0:
+        list.append(n)
+print(list)
---
+++ weather.py
@@ -0,0 +1,9 @@
+temp=int(input("please enter the current temprature   "))
+if temp >=30:
+    print("Its a hot day,stay hydrated")
+elif temp >= 20 and temp <= 29:
+    print("Its a ward day,Enjoy the weather")
+elif temp >= 10 and temp <= 19:
+    print("Its a cool day,Wear a jacket!")
+else:
+    print("Its a cold day, stay warm")
